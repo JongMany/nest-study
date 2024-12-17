fb361781ab9d63ffcd1932bb7c166f2a66eca498
JongMany <blackberry1114@naver.com>
2024-12-18 07:56:39 +0900

    feat: getMovies 로직을 서비스 레이어로 이동

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,83 +1,55 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  NotFoundException,
-  Param,
-  Patch,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Movie {
-  id: number;
-  title: string;
-}
 @Controller('movie')
 export class AppController {
-  private movies: Movie[] = [
-    {
-      id: 1,
-      title: '해리포터',
-    },
-    {
-      id: 2,
-      title: '반지의 제왕',
-    },
-  ];
-  private idCounter = 3;
-
   constructor(private readonly appService: AppService) {}
 
   @Get('')
   getMovies(@Query('title') title?: string) {
-    if (!title) return this.movies;
-
-    return this.movies.filter((movie) => movie.title.includes(title));
+    return this.appService.getManyMovies(title);
   }
 
-  @Get(':id')
-  getMovie(@Param('id') id: string) {
-    const movie = this.movies.find((movie) => movie.id === +id);
+  // @Get(':id')
+  // getMovie(@Param('id') id: string) {
+  //   const movie = this.movies.find((movie) => movie.id === +id);
 
-    if (!movie) {
-      throw new NotFoundException(`Movie with ID ${id} not found`);
-    }
-    return movie;
-  }
+  //   if (!movie) {
+  //     throw new NotFoundException(`Movie with ID ${id} not found`);
+  //   }
+  //   return movie;
+  // }
 
-  @Post('')
-  postMovie(@Body('title') title: string) {
-    const movie: Movie = {
-      id: this.idCounter++,
-      title: title,
-    };
+  // @Post('')
+  // postMovie(@Body('title') title: string) {
+  //   const movie: Movie = {
+  //     id: this.idCounter++,
+  //     title: title,
+  //   };
 
-    this.movies.push(movie);
-    return movie;
-  }
+  //   this.movies.push(movie);
+  //   return movie;
+  // }
 
-  @Patch(':id')
-  patchMovie(@Param('id') id: string, @Body('title') title: string) {
-    const movie = this.movies.find((movie) => movie.id === +id);
+  // @Patch(':id')
+  // patchMovie(@Param('id') id: string, @Body('title') title: string) {
+  //   const movie = this.movies.find((movie) => movie.id === +id);
 
-    if (!movie) {
-      throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
-    }
+  //   if (!movie) {
+  //     throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
+  //   }
 
-    Object.assign(movie, { title });
-    return movie;
-  }
+  //   Object.assign(movie, { title });
+  //   return movie;
+  // }
 
-  @Delete(':id')
-  deleteMovie(@Param('id') id: string) {
-    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
-    if (movieIndex === -1) {
-      throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
-    }
-    this.movies.splice(movieIndex, 1);
-    return id;
-  }
+  // @Delete(':id')
+  // deleteMovie(@Param('id') id: string) {
+  //   const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
+  //   if (movieIndex === -1) {
+  //     throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
+  //   }
+  //   this.movies.splice(movieIndex, 1);
+  //   return id;
+  // }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Movie {
+  id: number;
+  title: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+  private idCounter = 3;
+  getManyMovies(title?: string): Movie[] {
+    if (!title) return this.movies;
+
+    return this.movies.filter((movie) => movie.title.includes(title));
   }
 }
