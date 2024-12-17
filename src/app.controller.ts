667109ebbce76f5c4e3b8370c27269937a76868b
JongMany <blackberry1114@naver.com>
2024-12-18 07:45:04 +0900

    feat: movie delete api 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -69,7 +69,12 @@ export class AppController {
   }
 
   @Delete(':id')
-  deleteMovie() {
-    return 3;
+  deleteMovie(@Param('id') id: string) {
+    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
+    if (movieIndex === -1) {
+      throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
+    }
+    this.movies.splice(movieIndex, 1);
+    return id;
   }
 }
