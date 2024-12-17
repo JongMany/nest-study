0722053d142b39782d20fc981154c544e3133910
JongMany <blackberry1114@naver.com>
2024-12-18 08:10:47 +0900

    feat: movie module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [],
+  // 다른 module을 module로 import 할 때,
+  imports: [MovieModule],
+  // controller
   controllers: [AppController],
+  // ioc inject
   providers: [AppService],
 })
 export class AppModule {}
