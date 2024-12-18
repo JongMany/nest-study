f8c98626fd4fbb5e20f9bb418d34a52c31ce0b06
JongMany <blackberry1114@naver.com>
2024-12-19 08:20:06 +0900

    class transformer 추가

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,37 +1,37 @@
 import {
   IsNotEmpty,
   IsOptional,
-  registerDecorator,
-  Validate,
-  ValidationArguments,
-  ValidationOptions,
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
+  // registerDecorator,
+  // Validate,
+  // ValidationArguments,
+  // ValidationOptions,
+  // ValidatorConstraint,
+  // ValidatorConstraintInterface,
 } from 'class-validator';
 
-@ValidatorConstraint()
-class PasswordValidator implements ValidatorConstraintInterface {
-  validate(
-    value: any,
-    validationArguments?: ValidationArguments,
-  ): Promise<boolean> | boolean {
-    return value.length > 4 && value.length < 8;
-  }
-  defaultMessage(validationArguments?: ValidationArguments): string {
-    return '비밀번호의 길이는 4~8자여야 합니다. 입력된 비밀번호 ($value)';
-  }
-}
+// @ValidatorConstraint()
+// class PasswordValidator implements ValidatorConstraintInterface {
+//   validate(
+//     value: any,
+//     validationArguments?: ValidationArguments,
+//   ): Promise<boolean> | boolean {
+//     return value.length > 4 && value.length < 8;
+//   }
+//   defaultMessage(validationArguments?: ValidationArguments): string {
+//     return '비밀번호의 길이는 4~8자여야 합니다. 입력된 비밀번호 ($value)';
+//   }
+// }
 
-function IsPasswordValid(validationOptions?: ValidationOptions) {
-  return function (object: Object, propertyName: string) {
-    registerDecorator({
-      target: object.constructor,
-      propertyName,
-      options: validationOptions,
-      validator: PasswordValidator,
-    });
-  };
-}
+// function IsPasswordValid(validationOptions?: ValidationOptions) {
+//   return function (object: Object, propertyName: string) {
+//     registerDecorator({
+//       target: object.constructor,
+//       propertyName,
+//       options: validationOptions,
+//       validator: PasswordValidator,
+//     });
+//   };
+// }
 
 export class UpdateMovieDto {
   @IsNotEmpty()
