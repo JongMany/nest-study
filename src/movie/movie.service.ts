b5ac1e0715efd0a6648b3c683d1453e4f82581c1
JongMany <blackberry1114@naver.com>
2024-12-21 21:42:16 +0900

    통계 쿼리, advanced option(LIKE) 적용

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -3,17 +3,27 @@ import { UpdateMovieDto } from './dto/update-movie.dto';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entity/movie.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 
 @Injectable()
 export class MovieService {
   constructor(
     @InjectRepository(Movie) private movieRepository: Repository<Movie>,
   ) {}
-  getManyMovies(title?: string) {
+  async getManyMovies(title?: string) {
     // 나중에 title filter 기능 추가
+    if (!title) {
+      return [
+        await this.movieRepository.find(),
+        await this.movieRepository.count(),
+      ];
+    }
 
-    return this.movieRepository.find();
+    return this.movieRepository.findAndCount({
+      where: {
+        title: Like(`%${title}%`),
+      },
+    });
   }
   async getMovieById(id: number) {
     const movie = await this.movieRepository.findOne({
