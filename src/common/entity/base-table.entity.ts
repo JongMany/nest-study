2dccbceef6d9e04ae1038a4ef675c81d626ee5d6
JongMany <blackberry1114@naver.com>
2024-12-27 08:05:53 +0900

    base table @Exclude 추가

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,12 +1,16 @@
+import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 export class BaseTable {
+  @Exclude()
   @CreateDateColumn()
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Exclude()
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude()
   version: number;
 }
