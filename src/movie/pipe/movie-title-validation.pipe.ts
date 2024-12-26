dcbdd00bb6567af61298eba8fc7810b01ed21c4c
JongMany <blackberry1114@naver.com>
2024-12-27 07:54:38 +0900

    커스텀 pipe 버그 수정

--- src/movie/pipe/movie-title-validation.pipe.ts
+++ src/movie/pipe/movie-title-validation.pipe.ts
@@ -8,6 +8,11 @@ import {
 @Injectable()
 export class MovieTitleValidationPipe implements PipeTransform<string, string> {
   transform(value: string, metadata: ArgumentMetadata): string {
+    // title이 없는 경우
+    if (!value) {
+      return value;
+    }
+
     // 만약 글자 길이가 2보다 작거나 같으면 에러
     if (value.length <= 2) {
       throw new BadRequestException('영화의 제목은 3자 이상 작성해주세요!');
