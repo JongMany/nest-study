8e1139aefc1a334b28e11cd92f5d0701819a71fe
JongMany <blackberry1114@naver.com>
2025-01-05 12:57:34 +0900

    파일 업로드 기능 common 모듈에 이동

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
+import { CommonController } from './common.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { join } from 'path';
+import { v4 } from 'uuid';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: join(process.cwd(), 'public', 'temp'),
+        filename: (req, file, callback) => {
+          const split = file.originalname.split('.');
+
+          let extension = 'mp4';
+
+          if (split.length > 1) {
+            extension = split[split.length - 1];
+          }
+
+          callback(null, `${v4()}_${Date.now()}.${extension}`);
+        },
+      }), // 서버 파일 시스템
+    }),
+  ],
+  controllers: [CommonController],
   providers: [CommonService],
   exports: [CommonService], // CommonModule을 import한 다른 Module에서도 CommonService, CommonController, CommonEntity를 import할 수 있도록 exports
 })
