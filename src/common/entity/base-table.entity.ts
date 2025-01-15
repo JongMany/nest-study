5d2b5bfc9f18eb28c917f2939d6f515e30f87343
JongMany <blackberry1114@naver.com>
2025-01-16 07:33:26 +0900

    ApiHideProperty 추가

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,16 +1,20 @@
+import { ApiHideProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 export class BaseTable {
   @Exclude()
+  @ApiHideProperty()
   @CreateDateColumn()
   createdAt: Date;
 
   @UpdateDateColumn()
   @Exclude()
+  @ApiHideProperty()
   updatedAt: Date;
 
   @VersionColumn()
   @Exclude()
+  @ApiHideProperty()
   version: number;
 }
