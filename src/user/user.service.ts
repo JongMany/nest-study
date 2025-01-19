c11cc8bc771cafc7a383e425fca899d5fa765dc5
JongMany <blackberry1114@naver.com>
2025-01-20 07:49:21 +0900

    userService create 로직 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,49 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entity/user.entity';
+import * as bcrypt from 'bcrypt';
+import { envVariableKeys } from 'src/common/const/env.const';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly configService: ConfigService,
   ) {}
-  create(createUserDto: CreateUserDto) {
-    return this.userRepository.save(createUserDto);
+  async create(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      throw new BadRequestException('이미 가입된 이메일입니다.');
+    }
+
+    // 암호화하기 (bcrypt)
+    const hash = await bcrypt.hash(
+      password,
+      this.configService.get<number>(envVariableKeys.hashRound),
+    );
+
+    await this.userRepository.save({ email, password: hash });
+
+    return this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
   }
 
   findAll() {
